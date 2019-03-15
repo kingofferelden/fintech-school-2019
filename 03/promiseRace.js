7febf6ecae56b3d19b3563bba11492540847d7ea
Dmitry Korolev <dmitry@korolev.dk>
2019-03-15 21:09:33 +0300

    Add homework 3

---
+++ 03/promiseRace.js
@@ -0,0 +1,10 @@
+/**
+ * Напишите функцию promiseRace(promises), поведение
+ * которой аналогично поведению Promise.race(promises).
+ *
+ * @param  {Promise[]} promises массив с исходными промисами
+ * @return {Promise}
+ */
+export const promiseRace = promises => {
+
+};
